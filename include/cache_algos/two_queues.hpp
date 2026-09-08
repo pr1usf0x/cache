22d1fec663ecfc61f79a7511fbb89ad30a0f08a3
Sergey Kuzminskii <foma@MacBook-Air-Sergey.local>
2026-09-08 16:24:34 -0700

    move struts in Class and add '\n'

--- include/cache_algos/two_queues.hpp
+++ include/cache_algos/two_queues.hpp
@@ -8,21 +8,6 @@
 
 #include "cache.hpp"
 
-enum class CacheType { kIn, kOut, kLru };
-
-template <typename Key, typename Tp>
-struct CacheNode {
-  Key key;
-  Tp val;
-};
-
-template <typename Key, typename Tp>
-struct ElInfo {
-  CacheType cache_type;
-  typename std::list<CacheNode<Key, Tp>>::iterator list_it;
-  typename std::list<Key>::iterator key_it;
-};
-
 template <typename Key, typename Tp>
 class TwoQueues : public Cache<Key, Tp> {
  public:
@@ -38,6 +23,7 @@ class TwoQueues : public Cache<Key, Tp> {
     if (hash_it == data_base_.end()) {
       return AbsoluteMiss(key, slow_get_page);
     }
+
     switch (hash_it->second.cache_type) {
       case CacheType::kLru:
         return LruHit(hash_it);
@@ -48,55 +34,81 @@ class TwoQueues : public Cache<Key, Tp> {
       default:
         break;
     }
+
     return {};
   }
 
  private:
+  enum class CacheType { kIn, kOut, kLru };
+
+  struct CacheNode {
+    Key key;
+    Tp val;
+  };
+
+  struct ElInfo {
+    CacheType cache_type;
+    typename std::list<CacheNode>::iterator list_it;
+    typename std::list<Key>::iterator key_it;
+  };
+
   size_t cache_cap_;
   size_t in_cap_;
   size_t out_cap_;
   size_t lru_cap_;
-  std::list<CacheNode<Key, Tp>> in_cache_;
+
+  std::list<CacheNode> in_cache_;
   std::list<Key> out_cache_;
-  std::list<CacheNode<Key, Tp>> lru_cache_;
-  std::unordered_map<Key, ElInfo<Key, Tp>> data_base_;
+  std::list<CacheNode> lru_cache_;
+  std::unordered_map<Key, ElInfo> data_base_;
 
   Tp AbsoluteMiss(const Key& key,
                   std::function<Tp(const Key& key)> slow_get_page) {
     Tp old_elem = slow_get_page(key);
+
     if (in_cache_.size() >= in_cap_) {
       auto node = in_cache_.back();
       in_cache_.pop_back();
+
       if (out_cache_.size() >= out_cap_) {
         Key out_key = out_cache_.back();
         out_cache_.pop_back();
         data_base_.erase(out_key);
       }
+
       auto pos = out_cache_.emplace(out_cache_.begin(), node.key);
       data_base_[node.key] = {CacheType::kOut, {}, pos};
     }
+
     auto pos = in_cache_.emplace(in_cache_.begin(), {key, old_elem});
     data_base_[key] = {CacheType::kIn, pos, {}};
+
     return old_elem;
   }
+
   Tp LruHit(
-      typename std::unordered_map<Key, ElInfo<Key, Tp>>::iterator hash_it) {
+      typename std::unordered_map<Key, ElInfo>::iterator hash_it) {
     auto list_it = hash_it->second.list_it;
     lru_cache_.splice(lru_cache_.begin(), lru_cache_, list_it);
+
     return lru_cache_.front().val;
   }
+
   Tp OutHit(const Key& key,
-            typename std::unordered_map<Key, ElInfo<Key, Tp>>::iterator hash_it,
+            typename std::unordered_map<Key, ElInfo>::iterator hash_it,
             std::function<Tp(const Key& key)> slow_get_page) {
     Tp old_elem = slow_get_page(key);
     out_cache_.erase(hash_it->second.key_it);
+
     if (lru_cache_.size() >= lru_cap_) {
       auto node = lru_cache_.back();
       lru_cache_.pop_back();
       data_base_.erase(node.key);
     }
+
     auto pos = lru_cache_.emplace(lru_cache_.begin(), {key, old_elem});
     data_base_[key] = {CacheType::kLru, pos, {}};
+
     return old_elem;
   }
 };
