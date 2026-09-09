ac6d8cf549a3318e0fb531de0d8b19d16a149289
Sergey Kuzminskii <foma@MacBook-Air-Sergey.local>
2026-09-09 13:28:59 -0700

    rename file 2.0

--- style.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Находим файлы .cpp и .hpp в папках src и include и форматируем их на месте
-find src include -type f \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
-
-echo "Форматирование файлов .cpp и .hpp завершено!"
