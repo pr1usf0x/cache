c06dafb76dac59d61f237981849d0e7e14daaa42
Sergey Kuzminskii <foma@MacBook-Air-Sergey.local>
2026-09-09 13:25:14 -0700

    rename file and create arc algorithm (cache)

---
+++ include/cache_algos/arc.hpp
@@ -0,0 +1,18 @@
+#ifndef ARC_HPP_
+#define ARC_HPP_
+
+#include "cache.hpp"
+
+template <typename Key, typename Tp>
+class Arc: public Cache<Key, Tp> {
+public:
+
+
+private:
+size_t ram_sz_;
+size_t storage_cap_;
+
+}
+
+
+#endif
