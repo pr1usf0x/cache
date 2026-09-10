a964576efcb4f8758e521e520d2d73d675d05908
priusf0x <priusfoxlittle@gmail.com>
2026-09-10 17:44:46 +0300

    Google test

--- src/tests/cache_algorithm.cpp
+++ src/tests/cache_algorithm.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 
 TEST(GoogleTest, Init) {
-  EXPECT_EQ(1, 0);
+  EXPECT_EQ(1, 1);
 }
+// TODO: добавить юнит тестрование конструктора
+// TODO: как то придумать как посимулировать какие нибудь базовые вещи 
+
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
