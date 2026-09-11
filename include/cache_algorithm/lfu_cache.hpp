ba205b5fb6826d9d3c4b89d105b84e7c85ef3322
Sergey Kuzminskii <foma@MacBook-Air-Sergey.local>
2026-09-10 17:12:47 -0700

    lfu_cache.cpp after clang format

--- include/cache_algorithm/lfu_cache.hpp
+++ include/cache_algorithm/lfu_cache.hpp
@@ -1,200 +1,210 @@
 #ifndef LFU_CACHE_HPP_
 #define LFU_CACHE_HPP_
 
-#include <list>
-#include <unordered_map>
 #include <functional>
 #include <iterator>
+#include <list>
 #include <ostream>
+#include <unordered_map>
 
 #include "cache.hpp"
 
 namespace cache {
 
 template <typename Key, typename Tp>
-class Lfu: public Cache<Key, Tp> {
-public:
-    explicit Lfu(size_t cache_cap): cache_cap_(cache_cap) {}
+class Lfu : public Cache<Key, Tp> {
+ public:
+  explicit Lfu(size_t cache_cap) : cache_cap_(cache_cap) {}
 
-    Tp LookUpUpdate(const Key& key,
+  Tp LookUpUpdate(const Key& key,
                   std::function<Tp(const Key& key)> slow_get_page) override {
-        ++(this->access_count_);
+    ++(this->access_count_);
 
-        auto hash_it = data_base_.find(key);
+    auto hash_it = data_base_.find(key);
 
-        if (hash_it != data_base_.end()) {
-            return MoveOldNode(key, hash_it);
-        }
-
-        ++(this->misses_count_);
-
-        if (cache_sz_ >= cache_cap_) {
-            ClearCache();
-        }
-
-        return AddNewElement(key, slow_get_page);
+    if (hash_it != data_base_.end()) {
+      return MoveOldNode(key, hash_it);
     }
 
-    void Dump(std::ostream& out) const {
-         out << "\n=== LEAST FREQUENTLY USED (LFU) ===\n";
+    ++(this->misses_count_);
 
-        DisplayListOfLists(out);
-
-        DisplayDatabase(out);
-
-        out << "===================================\n\n";
+    if (cache_sz_ >= cache_cap_) {
+      ClearCache();
     }
 
-private:
-    struct CacheNode {
-        Key key;
-        Tp val;
-        size_t counter;
-    };
+    return AddNewElement(key, slow_get_page);
+  }
 
-    using CacheList = std::list<CacheNode>;
-    using ElemIt = typename CacheList::iterator;
+  void Dump(std::ostream& out) const {
+    out << "\n=== LEAST FREQUENTLY USED (LFU) ===\n";
 
-    struct FreqInfo {
-        size_t freq_num;
-        CacheList cache_list;
+    DisplayListOfLists(out);
 
-        explicit FreqInfo(size_t freq_num): freq_num(freq_num) {}
-    };
+    DisplayDatabase(out);
 
-    using FreqList = std::list<FreqInfo>;
-    using ListIt = typename FreqList::iterator;
+    out << "===================================\n\n";
+  }
 
-    struct ElInfo {
-        ElemIt elem_it;
-        ListIt list_it;
-    };
+ private:
+  struct CacheNode {
+    Key key;
+    Tp val;
+    size_t counter;
+  };
 
-    static constexpr size_t kMinCounter = 1;
+  using CacheList = std::list<CacheNode>;
+  using ElemIt = typename CacheList::iterator;
 
-    size_t cache_sz_;
-    size_t cache_cap_;
+  struct FreqInfo {
+    size_t freq_num;
+    CacheList cache_list;
 
-    FreqList frequencies_;
-    std::unordered_map<Key, ElInfo> data_base_;
+    explicit FreqInfo(size_t freq_num) : freq_num(freq_num) {}
+  };
 
-    Tp MoveOldNode(const Key& key, typename std::unordered_map<Key, ElInfo>::iterator hash_it) {
-        ListIt& list_it = (hash_it->second).list_it;
-        ElemIt& elem_it = (hash_it->second).elem_it;
+  using FreqList = std::list<FreqInfo>;
+  using ListIt = typename FreqList::iterator;
 
-        ListIt next_list_it = std::next(list_it);
+  struct ElInfo {
+    ElemIt elem_it;
+    ListIt list_it;
+  };
 
-        size_t current_freq_num = list_it->freq_num;
-        size_t next_freq_num = (next_list_it == frequencies_.end() ? 0 : next_list_it->freq_num);
+  static constexpr size_t kMinCounter = 1;
 
-        if (next_freq_num == current_freq_num + 1) {
-            return MoveNode(key, list_it, next_list_it, elem_it);
-        }
-        return CreateAndMove(key, list_it, next_list_it, elem_it);
+  size_t cache_sz_;
+  size_t cache_cap_;
+
+  FreqList frequencies_;
+  std::unordered_map<Key, ElInfo> data_base_;
+
+  Tp MoveOldNode(const Key& key,
+                 typename std::unordered_map<Key, ElInfo>::iterator hash_it) {
+    ListIt& list_it = (hash_it->second).list_it;
+    ElemIt& elem_it = (hash_it->second).elem_it;
+
+    ListIt next_list_it = std::next(list_it);
+
+    size_t current_freq_num = list_it->freq_num;
+    size_t next_freq_num =
+        (next_list_it == frequencies_.end() ? 0 : next_list_it->freq_num);
+
+    if (next_freq_num == current_freq_num + 1) {
+      return MoveNode(key, list_it, next_list_it, elem_it);
+    }
+    return CreateAndMove(key, list_it, next_list_it, elem_it);
+  }
+
+  Tp MoveNode(const Key& key, ListIt& list_it, ListIt& next_list_it,
+              ElemIt& elem_it) {
+    Tp elem_val = elem_it->val;
+    ++(elem_it->counter);
+
+    CacheList& cache_list = list_it->cache_list;
+    CacheList& next_cache_list = next_list_it->cache_list;
+    next_cache_list.splice(next_cache_list.end(), cache_list, elem_it);
+
+    if (cache_list.empty()) {
+      frequencies_.erase(list_it);
     }
 
-    Tp MoveNode(const Key& key, ListIt& list_it, ListIt& next_list_it, ElemIt& elem_it) {
-        Tp elem_val = elem_it->val;
-        ++(elem_it->counter);
+    data_base_[key] = {elem_it, next_list_it};
+    return elem_val;
+  }
 
-        CacheList& cache_list = list_it->cache_list;
-        CacheList& next_cache_list = next_list_it->cache_list;
-        next_cache_list.splice(next_cache_list.end(), cache_list, elem_it);
+  Tp CreateAndMove(const Key& key, ListIt& list_it, ListIt& next_list_it,
+                   ElemIt& elem_it) {
+    Tp elem_val = elem_it->val;
+    ++(elem_it->counter);
 
-        if (cache_list.empty()) {
-            frequencies_.erase(list_it);
-        }
+    auto freq_it =
+        frequencies_.insert(next_list_it, FreqInfo{elem_it->counter});
 
-        data_base_[key] = {elem_it, next_list_it};
-        return elem_val;
+    CacheList& cache_list = list_it->cache_list;
+    CacheList& new_cache_list = freq_it->cache_list;
+    new_cache_list.splice(new_cache_list.end(), cache_list, elem_it);
+
+    if (cache_list.empty()) {
+      frequencies_.erase(list_it);
     }
 
-    Tp CreateAndMove(const Key& key, ListIt& list_it, ListIt& next_list_it, ElemIt& elem_it) {
-        Tp elem_val = elem_it->val;
-        ++(elem_it->counter);
+    data_base_[key] = {elem_it, freq_it};
+    return elem_val;
+  }
 
-        auto freq_it = frequencies_.insert(next_list_it, FreqInfo{elem_it->counter});
+  void ClearCache() {
+    CacheList& first_list = (frequencies_.begin())->cache_list;
+    Key delete_key = (first_list.front()).key;
 
-        CacheList& cache_list = list_it->cache_list;
-        CacheList& new_cache_list = freq_it->cache_list;
-        new_cache_list.splice(new_cache_list.end(), cache_list, elem_it);
+    first_list.pop_front();
 
-        if (cache_list.empty()) {
-            frequencies_.erase(list_it);
-        }
-
-        data_base_[key] = {elem_it, freq_it};
-        return elem_val;
+    if (first_list.empty()) {
+      frequencies_.erase(frequencies_.begin());
     }
 
-    void ClearCache() {
-        CacheList& first_list = (frequencies_.begin())->cache_list;
-        Key delete_key = (first_list.front()).key;
+    data_base_.erase(delete_key);
+    --cache_sz_;
+  }
 
-        first_list.pop_front();
+  Tp AddNewElement(const Key& key,
+                   std::function<Tp(const Key& key)> slow_get_page) {
+    Tp elem_val = slow_get_page(key);
+    ListIt first_list = frequencies_.begin();
 
-        if (first_list.empty()) {
-            frequencies_.erase(frequencies_.begin());
-        }
-
-        data_base_.erase(delete_key);
-        --cache_sz_;
+    if (first_list != frequencies_.end() &&
+        first_list->freq_num == kMinCounter) {
+      CacheList& cache_list = first_list->cache_list;
+      auto new_it = cache_list.emplace(cache_list.end(),
+                                       CacheNode{key, elem_val, kMinCounter});
+      data_base_[key] = {new_it, first_list};
+    } else {
+      CacheList new_list;
+      auto new_elem_it = new_list.emplace(
+          new_list.end(), CacheNode{key, elem_val, kMinCounter});
+      auto freq_it =
+          frequencies_.insert(frequencies_.begin(), FreqInfo{kMinCounter});
+      CacheList& freq_begin = freq_it->cache_list;
+      freq_begin.splice(freq_begin.end(), new_list, new_elem_it);
+      data_base_[key] = {new_elem_it, freq_it};
     }
 
-    Tp AddNewElement(const Key& key, std::function<Tp(const Key& key)> slow_get_page) {
-        Tp elem_val = slow_get_page(key);
-        ListIt first_list = frequencies_.begin();
+    ++cache_sz_;
+    return elem_val;
+  }
 
-        if (first_list != frequencies_.end() && first_list->freq_num == kMinCounter) {
-            CacheList& cache_list = first_list->cache_list;
-            auto new_it = cache_list.emplace(cache_list.end(), CacheNode{key, elem_val, kMinCounter});
-            data_base_[key] = {new_it, first_list};
-        } else {
-            CacheList new_list;
-            auto new_elem_it = new_list.emplace(new_list.end(), CacheNode{key, elem_val, kMinCounter});
-            auto freq_it = frequencies_.insert(frequencies_.begin(), FreqInfo{kMinCounter});
-            CacheList& freq_begin = freq_it->cache_list;
-            freq_begin.splice(freq_begin.end(), new_list, new_elem_it);
-            data_base_[key] = {new_elem_it, freq_it};
-        }
+  void DisplayListOfLists(std::ostream& out) const {
 
-        ++cache_sz_;
-        return elem_val;
+    out << "Total Capacity: " << cache_cap_
+        << " | Total Elements: " << cache_sz_ << '\n';
+
+    int num = 1;
+    for (auto it = frequencies_.begin(); it != frequencies_.end(); ++it) {
+      out << "\nList " << num << '\n';
+      DisplayList(out, it->cache_lit);
+      ++num;
     }
+  }
 
-    void DisplayListOfLists(std::ostream& out) const {
+  void DisplayList(std::ostream& out, const CacheList& list) const {
 
-        out << "Total Capacity: " << cache_cap_ << " | Total Elements: " << cache_sz_
-            << '\n';
-
-        int num = 1;
-        for (auto it = frequencies_.begin(); it != frequencies_.end(); ++it) {
-            out << "\nList " << num << '\n';
-            DisplayList(out, it->cache_lit);
-            ++num;
-        }
+    int num = 1;
+    for (auto it = list.begin(); it != list.end(); ++it) {
+      out << "\t\t[" << num << ']' << ": Key = " << it->key
+          << ", counter = " << it->counter << '\n';
+      ++num;
     }
+  }
 
-    void DisplayList(std::ostream& out, const CacheList& list) const {
+  void DisplayDatabase(std::ostream& out) {
+    out << "\n--- DATA BASE ---\n";
+    size_t num = 1;
 
-        int num = 1;
-        for (auto it = list.begin(); it != list.end(); ++it) {
-            out << "\t\t[" << num << ']' << ": Key = " << it->key << ", counter = " << it->counter << '\n';
-            ++num;
-        }
+    for (auto it = data_base_.begin(); it != data_base_.end(); ++it) {
+      out << num << ". Key: " << it->first << '\n';
+      ++num;
     }
-
-    void DisplayDatabase(std::ostream& out) {
-        out << "\n--- DATA BASE ---\n";
-        size_t num = 1;
-
-        for (auto it = data_base_.begin(); it != data_base_.end(); ++it) {
-            out << num << ". Key: " << it->first << '\n';
-            ++num;
-        }
-    }
+  }
 };
-} // namespace cache
-
+}  // namespace cache
 
 #endif
